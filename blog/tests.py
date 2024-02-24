82be456909aaf4a21904cc3f5a8fd242664bae86
blackxavier <ogbuchiarinze01@gmail.com>
2024-02-24 23:03:53 +0100

    Pulled

--- blog/tests.py
+++ blog/tests.py
@@ -1,12 +1,13 @@
 from django.test import TestCase
 from .models import Post
 
-# Create your tests here.
+
 class PostTests(TestCase):
+
     def setUp(self):
         self.blog = Post.objects.create(title='just a test', author='Arinze', slug='arinze-test')
         self.blog.save()
-        
+
     def test_post_content(self):
         self.assertEqual(self.blog.title, 'just a test')
         self.assertEqual(self.blog.author, 'Arinze')
