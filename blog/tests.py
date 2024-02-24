88e0cb86f0af21b3c8b1c946d2745a99a059b6dd
blackxavier <ogbuchiarinze01@gmail.com>
2024-02-24 22:46:54 +0100

    flake8 setup, preparing for actions

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from .models import Post
 
 # Create your tests here.
+class PostTests(TestCase):
+    def setUp(self):
+        self.blog = Post.objects.create(title='just a test', author='Arinze', slug='arinze-test')
+        self.blog.save()
+        
+    def test_post_content(self):
+        self.assertEqual(self.blog.title, 'just a test')
+        self.assertEqual(self.blog.author, 'Arinze')
+        self.assertEqual(self.blog.slug, 'arinze-test')
+        self.assertEqual(str(self.blog), 'just a test')
